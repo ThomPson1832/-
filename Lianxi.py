9ba7fba03dc70c496ec76f9d4ea761f18ce8e705
ThomPson1832 <123589765+ThomPson1832@users.noreply.github.com>
2023-05-16 17:22:57 +0800

    Create Lianxi.py

---
+++ Lianxi.py
@@ -0,0 +1,35 @@
+# 定义两个整数变量
+
+num1 = 10
+
+num2 = 5
+
+# 计算加法、减法、乘法和除法
+
+add = num1 + num2
+
+sub = num1 - num2
+
+mul = num1 * num2
+
+div = num1 / num2
+
+# 输出结果
+
+print("加法结果：", add)
+
+print("减法结果：", sub)
+
+print("乘法结果：", mul)
+
+print("除法结果：", div)
+
+# 判断num1是否大于num2
+
+if num1 > num2:
+
+    print("num1大于num2")
+
+else:
+
+    print("num1小于等于num2")
